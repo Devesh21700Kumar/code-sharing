d41573f658e17a73a8ab79ddb160bd3150119790
Devesh <rockdev1234560@gmail.com>
2021-09-10 10:21:15 +0530

    initialised mongodb instance and setup the dev dependencies and packages

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+require('dotenv').config();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+
+async function connect(){
+	try{
+		await mongoose.connect(process.env.MONGOURL, { useUnifiedTopology: true, useNewUrlParser: true });
+		console.log("Connected to DB");
+	} catch(err){
+		console.log("Error while connecting to DB");
+		console.log(process.env.MONGOURL)
+		process.exit(0);
+	}
+}
+
+connect();
