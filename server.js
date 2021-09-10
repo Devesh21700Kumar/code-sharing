1c541b2fe70f844c7970f3b44d4e7e0621e11b50
Devesh <rockdev1234560@gmail.com>
2021-09-10 10:31:21 +0530

    the folder structure and the client side files

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 require('dotenv').config();
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
