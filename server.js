aebbef3820674938c6e14c056ec66d59950ab049
Devesh <rockdev1234560@gmail.com>
2021-09-10 10:43:20 +0530

    complete the styles and teh client side

--- server.js
+++ server.js
@@ -22,3 +22,48 @@ async function connect(){
 }
 
 connect();
+
+app.get("/", (req, res) => {
+    const code = `Welcome to WasteBin!
+  Use the commands in the top right corner
+  to create a new file to share with others.`
+  
+    res.render("code-display", { code, language: "plaintext" })
+  })
+  
+  app.get("/new", (req, res) => {
+    res.render("new")
+  })
+  
+  app.post("/save", async (req, res) => {
+    const value = req.body.value
+    try {
+      const document = await Document.create({ value })
+      res.redirect(`/${document.id}`)
+    } catch (e) {
+      res.render("new", { value })
+    }
+  })
+  
+  app.get("/:id/duplicate", async (req, res) => {
+    const id = req.params.id
+    try {
+      const document = await Document.findById(id)
+      res.render("new", { value: document.value })
+    } catch (e) {
+      res.redirect(`/${id}`)
+    }
+  })
+  
+  app.get("/:id", async (req, res) => {
+    const id = req.params.id
+    try {
+      const document = await Document.findById(id)
+  
+      res.render("code-display", { code: document.value, id })
+    } catch (e) {
+      res.redirect("/")
+    }
+  })
+  
+  app.listen(3000)
